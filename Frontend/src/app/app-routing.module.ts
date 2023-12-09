6e4539c7abde8d2db2454f205ab8f3f1a5f1b5c6
MeliiG <meli.bogar15@gmail.com>
2023-12-08 23:52:25 -0500

    Finalizacion Modulo 1 - Base sesion

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 // importancion de los componentes
 import{Modulo1Component}from './components/modulo1/modulo1.component';
 import {Page404Component} from './components/page404/page404.component';
+import { SessionComponent } from './components/session/session.component';
 
 const routes: Routes = [
-
-  { path:'perfiles',component:Modulo1Component },
+  {path:'session',component:SessionComponent },
+  {path:'perfiles',component:Modulo1Component },
   {path:'404',component:Page404Component},
   {path:'**',redirectTo: '404',pathMatch: 'full'}
 
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,29 +10,47 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Modulo1Component } from './components/modulo1/modulo1.component';
 import { Page404Component } from './components/page404/page404.component';
+import {SessionComponent} from './components/session/session.component'
 
 //componentes de angular material
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     Modulo1Component,
-    Page404Component
+    Page404Component,
+    SessionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+
+
 
   ],
   providers: [],
